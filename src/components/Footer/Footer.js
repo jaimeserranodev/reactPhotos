72aed5b1fd94b68f0dd8db1ea663dc53a4b78719
jaimeserranodev <jaime.serrano.dev@gmail.com>
2023-06-06 10:46:58 +0100

    Modificado footer y header

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+
 
 export default function LabelBottomNavigation() {
 const [value, setValue] = React.useState('recents');
@@ -14,29 +10,24 @@ const handleChange = (event, newValue) => {
 };
 
 return (
-    <div style={{backgroundColor: '#0097A7'}}>
+    <div style={{backgroundColor: '#0097A7', }}>
     <BottomNavigation sx={{
         width: 500,
         margin: '50px auto 0px auto',
         background: '#E0F2F1',
-        borderRadius: '15px'
+        borderRadius: '15px',
+        display: 'flex', justifyContent: 'center', alignItems: 'center',
         }} value={value} onChange={handleChange}>
-    <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-    />
-    <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-    />
-    <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-    />
-    <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    <p style={{
+        fontFamily:'Leckerli One',
+        color:'#0097A7',
+        fontSize: '25px',
+        textAlign: 'center',
+        alignItems: 'center'
+
+
+    }}>
+        React Photos</p>
     </BottomNavigation>
     </div>
 );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,81 +3,86 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-
 import SearchIcon from '@mui/icons-material/Search';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
+
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-// import { sortBy } from "../../features/favorite/favoriteSlice";
 import { searchPhoto } from "../../features/search/searchSlice";
-// import { sortSearch } from "../../features/search/searchSlice";
 import { searchFAvorite } from "../../features/favorite/favoriteSlice";
 
 
 
 const Search = styled('div')(({ theme }) => ({
-position: 'relative',
-borderRadius: theme.shape.borderRadius,
-backgroundColor: alpha(theme.palette.common.white, 0.15),
-'&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-},
-marginLeft: 0,
-width: '100%',
-[theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-},
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+        marginLeft: 0,
+        width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-padding: theme.spacing(0, 2),
-height: '100%',
-position: 'absolute',
-pointerEvents: 'none',
-display: 'flex',
-alignItems: 'center',
-justifyContent: 'center',
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-color: 'inherit',
-'& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-    width: '12ch',
-    '&:focus': {
-        width: '20ch',
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+            width: '20ch',
+        },
+        },
     },
-    },
-},
 }));
 
 const SearchAppBar = () => {
     const [search, setSearch] = useState('');
     const dispatch = useDispatch();
-  
+
     const searchAction = (ev) => {
-      setSearch(ev.target.value);
-      dispatch(searchPhoto({ search }));
-      dispatch(searchFAvorite({ search }));
+        setSearch(ev.target.value);
+        dispatch(searchPhoto({ search }));
+        dispatch(searchFAvorite({ search }));
     };
-  
+
     const theme = createTheme({
-      palette: {
+        palette: {
         primary: {
-          main: '#0097A7',
+            main: '#0097A7',
         },
-      },
-    });
+    },
+    typography: {
+        fontFamily: [
+            'Leckerli One',
+            'Open Sans',
+            'Arial',
+            'sans-serif'
+        ].join(','),
+    },
+});
 
 
 return (
@@ -117,9 +122,9 @@ return (
                         <SearchIcon />
                     </SearchIconWrapper>
                     <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                    onChange={(ev) => searchAction(ev)}
+                        placeholder="Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                        onChange={(ev) => searchAction(ev)}
                     />
                 </Search>
             </Toolbar>
@@ -129,67 +134,3 @@ return (
 );
 };
 export default  SearchAppBar;
-
-
-
-// import { React, useState } from "react";
-// import SearchIcon from '@mui/icons-material/Search';
-// import { useDispatch } from "react-redux";
-// import { sortBy } from "../features/favorite/favoriteSlice";
-// import { searchPhoto } from "../features/search/searchSlice";
-// import { sortSearch } from "../features/search/searchSlice";
-// import { searchFAvorite } from "../features/favorite/favoriteSlice";
-
-// function SearchOrder() {
-
-//     let [search, setSearch] = useState('')
-
-//     let dispatch = useDispatch()
-
-//     // let onSort = (e) => {
-//     //     dispatch(sortBy(e.target.value))
-//     //     dispatch(sortSearch(e.target.value))
-//     // }
-
-//     let searchAction = (ev) =>{
-//         setSearch(ev.target.value)
-//         dispatch(searchPhoto({search: search}))
-//         dispatch(searchFAvorite({ search: search }))
-//     }
-
-
-
-//     return (
-//         <div style={{
-//             display: 'flex',
-//             justifyContent: 'space-between',
-//             paddingLeft: '5%',
-//             flexWrap: 'wrap'
-//         }}>
-//             <div>
-//                 {/* <label htmlFor="" style={{ marginRight: '5px' }}>Sort by</label>
-//                 <select name="filter" id="" placeholder="Filter" onChange={(target) => {onSort(target)}}>
-//                     <option value="none">None</option>
-//                     <option value="width">Width</option>
-//                     <option value="height">Height</option>
-//                     <option value="likes">Likes</option>
-//                     <option value="date">Date</option>
-//                 </select>
-//             </div>
-//             <div style={{
-//                 display: 'flex',
-//                 marginRight: '5%'
-//             }}> */}
-//                 <input onChange={(ev) => searchAction(ev)} type="text" placeholder="Search for description" style={{
-//                     borderRadius: '5px',
-//                     padding: '2px 4px',
-//                     width: '300px',
-//                 }} />
-//                 {/* <SearchIcon onClick={() => searchAction()} style={{background: '#900c3f', color: 'white'}}/> */}
-                
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default SearchOrder
